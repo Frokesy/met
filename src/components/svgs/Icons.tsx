7f032b21f4a7ce76790c7a32f9c771f797eaad64
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-07-13 03:52:34 +0100

    scaffolded AuthContainer component

---
+++ src/components/svgs/Icons.tsx
@@ -0,0 +1,18 @@
+export const BgAttachment = () => (
+  <svg
+    width="868"
+    height="1024"
+    viewBox="0 0 868 1024"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="867.61"
+      y="1087.4"
+      width="807.922"
+      height="1368.28"
+      transform="rotate(159 867.61 1087.4)"
+      fill="#16BBE5"
+    />
+  </svg>
+);
