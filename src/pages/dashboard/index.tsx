0d40a773823d20f0cbee9cb304b561c5942e2558
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-07-18 06:04:57 +0100

    modified dashboard component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,79 +1,66 @@
+import { Clock, FileWarning, ShieldUser, UserPen } from "lucide-react";
 import MainContainer from "../../components/containers/MainContainer";
-import {
-  AttendanceIcon,
-  AvatarIcon,
-  CGPAIcon,
-  MoneyIcon,
-} from "../../components/svgs/Icons";
 
 const Dashboard = () => {
-  const topPanelItems = [
-    {
-      id: 1,
-      title: "CGPA",
-      value: "8.5",
-      icon: <CGPAIcon />,
-    },
-    {
-      id: 2,
-      title: "Attendance",
-      value: "85%",
-      icon: <AttendanceIcon />,
-    },
-    {
-      id: 3,
-      title: "Dues",
-      value: "12000",
-      icon: <MoneyIcon />,
-    },
-  ];
   return (
     <MainContainer active="dashboard">
-      <div className="px-6 pt-10 text-[#2E4765]">
-        <div className="w-[100%] p-4 bg-[#fff] shadow-md rounded-xl border border-[#ccc]">
-          <h2 className="text-[24px]">Welcome John</h2>
-          <div className="px-10 grid grid-cols-3 gap-10 mt-10 mb-20">
-            {topPanelItems.map((item) => (
-              <div
-                key={item.id}
-                className="flex space-x-6 px-10 py-3 rounded-[30px] items-center border-2 border-[#16BBE5] hover:shadow-none shadow-[0_0_12px_#16BBE5] transition-all duration-300 ease-in-out"
-              >
-                {item.icon}
-                <div className="text-center">
-                  <p className="text-[16px] font-bold">{item.title}</p>
-                  <p className="text-[24px] font-bold">{item.value}</p>
-                </div>
+      <div className="px-6 pt-10">
+        <span className="text-[20px] text-gray-500 font-semibold">
+          Home / Dashboard
+        </span>
+        <div className="grid grid-cols-4 gap-6 w-[100%] mt-10">
+          <div className="bg-gray-800 border border-cyan-600 p-6 rounded-xl">
+            <div className="flex items-center justify-between">
+              <h2>Active Personnel</h2>
+              <div className="p-2 rounded-lg bg-gray-700">
+                <ShieldUser />
               </div>
-            ))}
+            </div>
+            <h2 className="text-[40px] mt-6">1</h2>
+            <span className="text-cyan-600">from database</span>
+          </div>
+          <div className="bg-gray-800 border border-cyan-600 p-6 rounded-xl">
+            <div className="flex items-center justify-between">
+              <h2>Pending Verification</h2>
+              <div className="p-2 rounded-lg bg-gray-700">
+                <UserPen />
+              </div>
+            </div>
+            <h2 className="text-[40px] mt-6">0</h2>
+            <span className="text-yellow-600">needs attention</span>
+          </div>
+          <div className="bg-gray-800 border border-cyan-600 p-6 rounded-xl">
+            <div className="flex items-center justify-between">
+              <h2>Today's Attendance</h2>
+              <div className="p-2 rounded-lg bg-gray-700">
+                <Clock />
+              </div>
+            </div>
+            <h2 className="text-[40px] mt-6">0.0%</h2>
+            <span className="text-red-600">below target</span>
+          </div>
+          <div className="bg-gray-800 border border-cyan-600 p-6 rounded-xl">
+            <div className="flex items-center justify-between">
+              <h2>System Alerts</h2>
+              <div className="p-2 rounded-lg bg-gray-700">
+                <FileWarning />
+              </div>
+            </div>
+            <h2 className="text-[40px] mt-6">0</h2>
+            <span className="text-green-600">in last 24 hours</span>
           </div>
         </div>
-        <div className="flex justify-between space-x-10 mt-10">
-          <div className="w-[50%] p-4  bg-[#fff] shadow-md rounded-xl border border-[#ccc]">
-            <h2 className="text-[24px]">Your Profile</h2>
-            <div className="mt-10 flex flex-col items-center justify-center">
-              <AvatarIcon size={200} />
-              <p className="text-[24px] mt-6">John</p>
-              <p className="text-[24px]">961321104000</p>
-            </div>
+
+        <div className="space-y-6 mt-10 w-[70%]">
+          <div className="bg-gray-800 border border-cyan-600 pt-6 px-6 pb-12 rounded-xl flex justify-between items-center">
+            <h2 className="text-[18px] font-semibold">Recent Activity</h2>
+            <span className="text-gray-500">View All</span>
           </div>
-          <div className="w-[50%] p-4  bg-[#fff] shadow-md rounded-xl border border-[#ccc]">
-            <h2 className="text-[24px]">Personal Details</h2>
-            <div className="mt-10 grid grid-cols-2 px-4">
-              <div className="space-y-6 text-[18px]">
-                <p>DOB: 01.04.2000</p>
-                <p>Gender: Male</p>
-                <p>Department: CSE</p>
-                <p>Year/sem: Third/06</p>
-                <p>Batch: 2020 - 2024</p>
-              </div>
-              <div className="space-y-6 text-[18px]">
-                <p>Arrears: 4</p>
-                <p>Degree: B.E</p>
-                <p>Email: xyz@gmail.com</p>
-                <p>Mobile: 123456789</p>
-                <p>Accommodation: Day Scholar</p>
-              </div>
-            </div>
+          <div className="bg-gray-800 border border-cyan-600 pt-6 px-6 pb-12 rounded-xl flex justify-between items-center">
+            <h2 className="text-[18px] font-semibold">
+              Current Duty Assignments
+            </h2>
+            <span className="text-gray-500">View All</span>
           </div>
         </div>
       </div>
